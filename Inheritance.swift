a57b727d165f0a08fdb832eebc06ada5bed7463a
RafeAaron16 <rafedrew7@gmail.com>
2024-06-28 13:25:11 +0300

    Inheritance

--- Inheritance.swift
+++ Inheritance.swift
@@ -68,7 +68,7 @@ class Car: Vehicle {
     var gear = 1
 
     override var description: String {
-        return super.description += " in gear \(gear)"
+        return super.description + " in gear \(gear)"
     }
 }
 
